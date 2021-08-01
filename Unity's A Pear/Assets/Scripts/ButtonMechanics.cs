7b2c1c8c5f2d2dc1b846ceb13a696e893d03996a
oneOlive <ogmunro@gmail.com>
2021-07-31 23:31:17 -0500

    button stuff
    - button currently can be pushed down once and it unlocks the door
    - the button does not come back up
    - snazzy animation

--- Unity's A Pear/Assets/Scripts/ButtonMechanics.cs
+++ Unity's A Pear/Assets/Scripts/ButtonMechanics.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class ButtonMechanics : MonoBehaviour
 {
+    public bool staysPressed;
     public bool isPressed;
     public Animator buttonPress;
     public Animator buttonEffect;
@@ -11,6 +12,7 @@ public class ButtonMechanics : MonoBehaviour
     void Start()
     {
         isPressed = false;
+        // staysPressed = true;
     }
 
     void OnCollisionEnter2D(Collision2D collision)
@@ -21,6 +23,33 @@ public class ButtonMechanics : MonoBehaviour
             buttonPress.SetTrigger("ButtonPressed");
             buttonEffect.SetTrigger("ButtonEffect");
             Debug.Log("ButtonPressed");
+            // Wait();
         }
+
+        // if(!staysPressed)
+        // {
+        //     OnCollisionExit2D();
+        // }
     }
+
+    // IEnumerator DoAnimation()
+    // {
+    //     Debug.Log("waiting");
+    //     buttonPress.SetTrigger("ButtonPressed");
+    //     buttonEffect.SetTrigger("ButtonEffect");
+    //     yield return new WaitForSeconds(3f);
+    // }
+        IEnumerator Wait(){
+            yield return new WaitForSeconds(3);
+        }
+
+    // void OnCollisionExit2D(Collision2D collision)
+    // {
+    //     if(collision.gameObject.tag != "Man")
+    //     {
+    //         buttonPress.SetTrigger("ButtonRelease");
+    //         isPressed = false;
+    //     }
+    //     Debug.Log("not being pressed");
+    // } 
 }
