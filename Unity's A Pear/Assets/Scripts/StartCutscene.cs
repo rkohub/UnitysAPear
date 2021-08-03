2782659ae87a001083b75b9ce9059c2eba53b3ac
oneOlive <ogmunro@gmail.com>
2021-08-03 17:03:49 -0500

    start of the end
    - fixed a few things in the darkness levels
    - added the skeleton for the ending cutscene

---
+++ Unity's A Pear/Assets/Scripts/StartCutscene.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartCutscene : MonoBehaviour
+{
+    public Animator cameraAnimation;
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "Man" || collision.tag == "Player")
+        {
+            cameraAnimation.SetBool("cutscene1", true);
+            Invoke(nameof(StopCutscene), 3f);
+        }
+    }
+
+    void StopCutscene()
+    {
+        cameraAnimation.SetBool("cutscene1", false);
+        Destroy(gameObject);
+    }
+}
